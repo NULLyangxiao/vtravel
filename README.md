4b9a39dc89cc7e0e8c0b74a31292388bb0cf622a
yangxiao123123 <2863702889.com>
2019-09-16 19:21:25 +0800

    a vue.js project

---
+++ README.md
@@ -0,0 +1,32 @@
+# vtravel
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+
+前端启动 npm run dev
+
+后端启动
+cd server
+npm run start
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      <span class="txt">输入城市/景点/游玩主题</span>
+    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <!-- {{this.doubleCity}} -->
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
+  </div>
+</template>
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  name: 'HomeHeader',
+  computed: {
+    ...mapState(['city']),
+    ...mapGetters(['doubleCity'])
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        padding-left: .16rem
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: 0.64rem
+      line-height: 0.64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc;
+      .txt
+        font-size: .3rem;
+        vertical-align: top;
+        margin-left: .01rem;
+    .header-right
+      min-width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+</style>
